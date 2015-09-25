9b72fe604d441af2e4aa7ae5afd46fd7dff54558
Andrew Chelladurai <theunknownandrew@gmail.com>
2015-09-25 09:09:54 +0530

    Clea irrelevant files from VCS tracking

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/andrewchelladurai/simplebible/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.andrewchelladurai.simplebible.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.andrewchelladurai.simplebible.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.0.5";
+}
