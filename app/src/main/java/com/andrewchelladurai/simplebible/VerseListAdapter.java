f7b887cf7a04ce3ae9805d11ad7497aa96e0575a
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-03-05 02:53:03 +0530

    Include Files affected by VerseListAdapter and Preferences.

---
+++ app/src/main/java/com/andrewchelladurai/simplebible/VerseListAdapter.java
@@ -0,0 +1,30 @@
+package com.andrewchelladurai.simplebible;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Andrew Chelladurai - TheUnknownAndrew[at]GMail[dot]com
+ * on 05-Mar-2016 @ 2:34 AM
+ */
+public class VerseListAdapter
+        extends ArrayAdapter<String> {
+
+    public VerseListAdapter(Context context, int resource, ArrayList<String> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
+        TextView textView = (TextView) view.findViewById(android.R.id.text1);
+        textView.setTypeface(Typeface.MONOSPACE);
+        return view;
+    }
+}
