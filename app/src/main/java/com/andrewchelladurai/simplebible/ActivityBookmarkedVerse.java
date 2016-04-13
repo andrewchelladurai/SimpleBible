b1644f2fbe5f7015ea3f7b97652e1d41cc15d8a0
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-04-14 00:29:45 +0530

    Update ID of UI components in activity_bookmarked_verse.

--- app/src/main/java/com/andrewchelladurai/simplebible/ActivityBookmarkedVerse.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/ActivityBookmarkedVerse.java
@@ -35,7 +35,7 @@ public class ActivityBookmarkedVerse
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookmarked_verse);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_bookmarked_verse_toolbar);
         setSupportActionBar(toolbar);
 
 /*
