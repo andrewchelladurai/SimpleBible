01664f2d74c79d04666d79e0da3ce8de39ef646c
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-04-29 00:37:28 +0530

    ActivityChapter - Event Handlers for Bottom Buttons are now in place.

--- app/src/main/java/com/andrewchelladurai/simplebible/ActivityChapter.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/ActivityChapter.java
@@ -26,10 +26,14 @@ package com.andrewchelladurai.simplebible;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ActivityChapter
-        extends AppCompatActivity {
+        extends AppCompatActivity
+        implements View.OnClickListener {
 
     public static String ARG_BOOK_NUMBER = "BOOK_NUMBER";
     public static String ARG_CHAPTER_NUMBER = "CHAPTER_NUMBER";
@@ -44,5 +48,57 @@ public class ActivityChapter
         if (null != getSupportActionBar()) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        bindButton(R.id.activity_chapter_but_previous, true);
+        bindButton(R.id.activity_chapter_but_notes, false);
+        bindButton(R.id.activity_chapter_but_chapter, false);
+        bindButton(R.id.activity_chapter_but_search, false);
+        bindButton(R.id.activity_chapter_but_next, true);
+    }
+
+    private void bindButton(int pButtonId, boolean pIsImageButton) {
+        if (pIsImageButton) {
+            AppCompatButton button = (AppCompatButton) findViewById(pButtonId);
+            if (null != button) {
+                button.setOnClickListener(this);
+            }
+        } else {
+            AppCompatImageButton button = (AppCompatImageButton) findViewById(pButtonId);
+            if (null != button) {
+                button.setOnClickListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View pView) {
+        switch (pView.getId()) {
+            case R.id.activity_chapter_but_previous: handlePreviousButtonClick(); break;
+            case R.id.activity_chapter_but_notes: handleNotesButtonClick(); break;
+            case R.id.activity_chapter_but_chapter: handleChapterButtonClick(); break;
+            case R.id.activity_chapter_but_search: handleSearchButtonClick(); break;
+            case R.id.activity_chapter_but_next: handleNextButtonClick(); break;
+            default: // FIXME: 29/4/16 This must be showcased
+        }
+    }
+
+    private void handleNextButtonClick() {
+
+    }
+
+    private void handleSearchButtonClick() {
+
+    }
+
+    private void handleChapterButtonClick() {
+
+    }
+
+    private void handleNotesButtonClick() {
+
+    }
+
+    private void handlePreviousButtonClick() {
+
     }
 }
