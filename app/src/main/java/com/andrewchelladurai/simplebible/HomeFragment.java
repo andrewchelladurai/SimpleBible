0b9273781ce820192a12fb4329e494c949fcb58d
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-05-15 01:39:41 +0530

    fragment_home - UI is now present.

--- app/src/main/java/com/andrewchelladurai/simplebible/HomeFragment.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/HomeFragment.java
@@ -6,11 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass. Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface to handle interaction events. Use
- * the {@link HomeFragment#newInstance} factory method to create an instance of this fragment.
- */
 public class HomeFragment
         extends Fragment {
 
