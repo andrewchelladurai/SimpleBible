90e02369d823289e4a71757b099729b11faf24e2
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-09-05 20:58:55 +0530

    Added Test Class, no tests implemented.

---
+++ app/src/test/java/com/andrewchelladurai/simplebible/PagerAdapterTest.java
@@ -0,0 +1,22 @@
+package com.andrewchelladurai.simplebible;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by Andrew Chelladurai - TheUnknownAndrew[at]GMail[dot]com on 05-Sep-2016 @ 7:43 PM
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class PagerAdapterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
