9728f4f1f0b6155c99a54f874a3c2400fdd3121c
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-09-05 20:58:08 +0530

    Removed method to get titles from presented and moved it into view.

--- app/src/main/java/com/andrewchelladurai/simplebible/SimpleBibleActivity.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/SimpleBibleActivity.java
@@ -1,24 +1,22 @@
 package com.andrewchelladurai.simplebible;
 
 import android.content.Context;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 import com.andrewchelladurai.simplebible.interaction.SimpleBibleActivityInterface;
@@ -76,7 +74,6 @@ public class SimpleBibleActivity
             case R.id.action_settings: return true;
             default: return super.onOptionsItemSelected(item);
         }
-
     }
 
     @Override public Context getThisApplicationContext() {
@@ -113,8 +110,26 @@ public class SimpleBibleActivity
     private class PagerAdapter
             extends FragmentPagerAdapter {
 
-        public PagerAdapter(FragmentManager fm) {
-            super(fm);
+        String mTitles[] = null;
+
+        /**
+         * Initializes the super class and also the titles to be used in the Tabs. A string-array
+         * "main_activity_tab_titles" is used for this. If the array is not present, the Tabs will
+         * have a single entry that uses the application_name resource string.
+         *
+         * @param fragmentManager
+         */
+        public PagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+            if (null == mTitles || mTitles.length == 0) {
+                mTitles = getResources().getStringArray(R.array.main_activity_tab_titles);
+                if (mTitles == null || mTitles.length == 0) {
+                    mTitles = new String[]{getString(R.string.application_name)};
+                    Log.d(TAG, "string-array \"main_activity_tab_titles\" not found");
+                }
+                Log.d(TAG, "Tab Titles populated with " + mTitles.length + " entries <= " +
+                           "This should ideally print only once during init.");
+            }
         }
 
         @Override
@@ -124,19 +139,12 @@ public class SimpleBibleActivity
 
         @Override
         public int getCount() {
-            return mPresenter.getTabsCount();
+            return mTitles.length;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            String titles[] = mPresenter.getTabTitles();
-            int titleCount = titles.length;
-            Log.d(TAG, "getPageTitle() called : position = [" + position + "] " +
-                       "titleCount = [" + titleCount + "]");
-            if (position < 0 | position > titleCount) {
-                return getString(R.string.application_name);
-            }
-            return titles[position];
+            return mTitles[position];
         }
     }
 }
--- app/src/main/java/com/andrewchelladurai/simplebible/presentation/SimpleBibleActivityPresenter.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/presentation/SimpleBibleActivityPresenter.java
@@ -12,8 +12,7 @@ import com.andrewchelladurai.simplebible.interaction.SimpleBibleActivityInterfac
 public class SimpleBibleActivityPresenter {
 
     private static final String TAG = "SB_SBActivityPresenter";
-    private SimpleBibleActivityInterface mAInterface;
-    private String[] mTabTitles = null;
+    private SimpleBibleActivityInterface mInterface;
 
     /**
      * This must be called in the onCreate of the Activity and also before any of the views is
@@ -21,7 +20,7 @@ public class SimpleBibleActivityPresenter {
      * Reason being, this constructor will init the DB connection etc...
      */
     public SimpleBibleActivityPresenter(SimpleBibleActivityInterface aInterface) {
-        mAInterface = aInterface;
+        mInterface = aInterface;
         init();
     }
 
@@ -32,30 +31,4 @@ public class SimpleBibleActivityPresenter {
         Log.d(TAG, "init: called");
     }
 
-    /**
-     * Returns the count of Tabs to show on the activity.
-     * Calculates based on the items in the string array defined main_activity_tab_titles
-     * @return count of Tabs to show on the activity.
-     */
-    public int getTabsCount() {
-        return getTabTitles().length;
-    }
-
-    /**
-     * Returns the items in the string array defined main_activity_tab_titles
-     * If the array is not defined only one entry with the Application Name is returned
-     * @return an array of texts defined to be used as titles on Tabs.
-     */
-    public String[] getTabTitles() {
-        if (null == mTabTitles || mTabTitles.length == 0) {
-            Context context = mAInterface.getThisApplicationContext();
-            mTabTitles = context.getResources().getStringArray(R.array.main_activity_tab_titles);
-            if (mTabTitles == null || mTabTitles.length == 0) {
-                mTabTitles = new String[]{context.getString(R.string.application_name)};
-                Log.d(TAG, "getTabTitles: mTabTitles main_activity_tab_titles not found");
-            }
-            Log.d(TAG, "getTabTitles: initialized");
-        }
-        return mTabTitles;
-    }
 }
