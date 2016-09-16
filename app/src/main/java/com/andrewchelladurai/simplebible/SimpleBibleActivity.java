04415899484a02c068d12a4e64cca25b269a2e3c
Andrew Chelladurai <theunknownandrew@gmail.com>
2016-09-17 02:08:21 +0530

    Remove the FAB from the main activity
     Also now moved the presenter init inside the init() method

--- app/src/main/java/com/andrewchelladurai/simplebible/SimpleBibleActivity.java
+++ app/src/main/java/com/andrewchelladurai/simplebible/SimpleBibleActivity.java
@@ -2,8 +2,6 @@ package com.andrewchelladurai.simplebible;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -12,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.andrewchelladurai.simplebible.adapter.TabsAdapter;
 import com.andrewchelladurai.simplebible.interaction.SimpleBibleActivityOperations;
@@ -30,8 +27,6 @@ public class SimpleBibleActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // init the presenter so it can start the necessary DB and Alarm services.
-        mPresenter = new SimpleBibleActivityPresenter(this);
         init();
         setContentView(R.layout.activity_simple_bible);
 
@@ -46,21 +41,14 @@ public class SimpleBibleActivity
         TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_simple_bible_tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab =
-            (FloatingActionButton) findViewById(R.id.activity_simple_bible_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
         Log.d(TAG, "onCreate() returned");
     }
 
     @Override
     public void init() {
         Log.d(TAG, "init() called");
+        // init the presenter so it can start the necessary DB and Alarm services.
+        mPresenter = new SimpleBibleActivityPresenter(this);
         mPresenter.init();
     }
 
