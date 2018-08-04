b1aad70927827ddc0a359bb9ea5279f6288bd103
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-04 23:45:27 +0530

    UIX - Create Basic Splash Activity

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleActivity.java
@@ -0,0 +1,33 @@
+package com.andrewchelladurai.simplebible.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.andrewchelladurai.simplebible.R;
+
+public class SimpleBibleActivity
+    extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SbTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_simplebible);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
