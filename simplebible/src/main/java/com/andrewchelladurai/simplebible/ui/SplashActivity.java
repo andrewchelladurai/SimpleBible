49b75c45cf2f551a3f92751691ceff216ef35eab
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-08 23:48:28 +0530

    Feat - Create Empty Splash Activity

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.andrewchelladurai.simplebible.ui;
+
+import android.os.Bundle;
+
+import com.andrewchelladurai.simplebible.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity
+    extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
