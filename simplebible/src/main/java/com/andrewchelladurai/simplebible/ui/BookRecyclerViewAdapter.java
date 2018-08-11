c264ba1c7ce3aeda0fbef5295a43b8bc18a0b0bf
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-11 18:12:19 +0530

    Feat - Create basic BooksListFragment

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/BookRecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.andrewchelladurai.simplebible.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.andrewchelladurai.simplebible.R;
+import com.andrewchelladurai.simplebible.ui.dummy.DummyContent;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class BookRecyclerViewAdapter
+    extends RecyclerView.Adapter<BookRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyContent.DummyItem>                   mValues;
+    private final BookFragment.OnListFragmentInteractionListener mListener;
+
+    public BookRecyclerViewAdapter(List<DummyContent.DummyItem> items,
+                                   BookFragment.OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.item_book, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder
+        extends RecyclerView.ViewHolder {
+
+        public final View                   mView;
+        public final TextView               mIdView;
+        public final TextView               mContentView;
+        public       DummyContent.DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
