df26dd6848f41697415f3bfa93d4ed111f304ff3
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-13 01:03:29 +0530

    Feat - Create methods to get chapter, verse or verses with text

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
@@ -2,7 +2,11 @@ package com.andrewchelladurai.simplebible.data.dao;
 
 import com.andrewchelladurai.simplebible.data.entities.Verse;
 
+import java.util.List;
+
+import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -25,4 +29,19 @@ public interface VerseDao {
 
     @Query("delete from BIBLEVERSES")
     void deleteAllRecords();
+
+    @Query(
+        "select * from BIBLEVERSES where BOOKNUMBER = :book and CHAPTERNUMBER = :chapter order by VERSENUMBER ASC")
+    LiveData<List<Verse>> getChapter(@IntRange(from = 1, to = 66) int book,
+                                     @IntRange(from = 1) int chapter);
+
+    @Query(
+        "select * from BIBLEVERSES where BOOKNUMBER = :book and CHAPTERNUMBER = :chapter and VERSENUMBER = :verse order by VERSENUMBER ASC")
+    LiveData<Verse> getVerse(@IntRange(from = 1, to = 66) int book,
+                             @IntRange(from = 1) int chapter,
+                             @IntRange(from = 1) int verse);
+
+    @Query(
+        "select * from BIBLEVERSES where VERSETEXT LIKE :text order by BOOKNUMBER, CHAPTERNUMBER , VERSENUMBER ASC")
+    LiveData<List<Verse>> getVerseWithText(@NonNull String text);
 }
