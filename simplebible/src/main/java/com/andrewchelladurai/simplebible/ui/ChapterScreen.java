a38e8f697f860b23b75afbcb3bac0aa75708f172
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-13 01:14:51 +0530

    Feat - Create empty Chapter Screen

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ChapterScreen.java
@@ -0,0 +1,17 @@
+package com.andrewchelladurai.simplebible.ui;
+
+import android.os.Bundle;
+
+import com.andrewchelladurai.simplebible.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChapterScreen
+    extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter);
+    }
+}
