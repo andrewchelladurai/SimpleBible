eb9a3fe3902c980f02ff4c1b34b66741b4f277ea
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-16 19:04:02 +0530

    Feat - Create Abstract BaseRepository extending AndroidViewModel and implementing RepositoryOps

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/data/repository/BaseRepository.java
@@ -0,0 +1,24 @@
+package com.andrewchelladurai.simplebible.data.repository;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+/**
+ * Created by : Andrew Chelladurai
+ * Email : TheUnknownAndrew[at]GMail[dot]com
+ * on : 11-Aug-2018 @ 7:31 PM.
+ */
+abstract class BaseRepository
+    extends AndroidViewModel
+    implements RepositoryOps {
+
+    BaseRepository(final Application application) {
+        super(application);
+    }
+
+    abstract void clearCache();
+
+    abstract boolean isCacheValid(@NonNull Object... cacheParams);
+}
