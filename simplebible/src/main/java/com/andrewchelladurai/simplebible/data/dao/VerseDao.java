8ce82cf1802d4e096d4e77ec9d8be9235e5beeea
Andrew Chelladurai <TheUnknownAndrew@gmail.com>
2018-08-16 22:24:43 +0530

    Feat - Create method to get a list of verses using Lists

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
@@ -21,7 +21,8 @@ import androidx.room.Query;
 @Dao
 public interface VerseDao {
 
-    @Query("select distinct count(BOOKNUMBER||CHAPTERNUMBER||VERSENUMBER) from BIBLEVERSES")
+    @Query("select distinct count(BOOKNUMBER||CHAPTERNUMBER||VERSENUMBER)"
+           + " from BIBLEVERSES")
     int getRecordCount();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
@@ -30,18 +31,33 @@ public interface VerseDao {
     @Query("delete from BIBLEVERSES")
     void deleteAllRecords();
 
-    @Query(
-        "select * from BIBLEVERSES where BOOKNUMBER = :book and CHAPTERNUMBER = :chapter order by VERSENUMBER ASC")
+    @Query("select * from BIBLEVERSES"
+           + " where BOOKNUMBER = :book"
+           + " and CHAPTERNUMBER = :chapter"
+           + " order by VERSENUMBER ASC")
     LiveData<List<Verse>> getChapter(@IntRange(from = 1, to = 66) int book,
                                      @IntRange(from = 1) int chapter);
 
-    @Query(
-        "select * from BIBLEVERSES where BOOKNUMBER = :book and CHAPTERNUMBER = :chapter and VERSENUMBER = :verse order by VERSENUMBER ASC")
+    @Query("select * from BIBLEVERSES"
+           + " where BOOKNUMBER = :book"
+           + " and CHAPTERNUMBER = :chapter"
+           + " and VERSENUMBER = :verse"
+           + " order by VERSENUMBER ASC")
     LiveData<Verse> getVerse(@IntRange(from = 1, to = 66) int book,
                              @IntRange(from = 1) int chapter,
                              @IntRange(from = 1) int verse);
 
-    @Query(
-        "select * from BIBLEVERSES where VERSETEXT LIKE :text order by BOOKNUMBER, CHAPTERNUMBER , VERSENUMBER ASC")
+    @Query("select * from BIBLEVERSES"
+           + " where VERSETEXT LIKE :text"
+           + " order by BOOKNUMBER, CHAPTERNUMBER , VERSENUMBER ASC")
     LiveData<List<Verse>> getVerseWithText(@NonNull String text);
+
+    @Query("select * from BIBLEVERSES"
+           + " where BOOKNUMBER in (:bookList)"
+           + " and CHAPTERNUMBER in (:chapterList)"
+           + " and VERSENUMBER in (:verseList)"
+           + " order by BOOKNUMBER, CHAPTERNUMBER , VERSENUMBER ASC")
+    LiveData<List<Verse>> getVerses(@NonNull List<Integer> bookList,
+                                    @NonNull List<Integer> chapterList,
+                                    @NonNull List<Integer> verseList);
 }
