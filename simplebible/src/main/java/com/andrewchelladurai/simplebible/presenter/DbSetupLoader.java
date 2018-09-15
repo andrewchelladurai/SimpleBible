11a8abfcb535d270af377fc52d86ccca3fbe8922
Andrew Chelladurai <theunknownandrew@gmail.com>
2018-09-15 18:01:36 +0530

    Feat - Create Loader to create database

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/presenter/DbSetupLoader.java
@@ -0,0 +1,186 @@
+/*
+ *
+ * This file 'DbSetupLoader.java' is part of SimpleBible :
+ *
+ * Copyright (c) 2018.
+ *
+ * This Application is available at below locations
+ * Binary : https://play.google.com/store/apps/developer?id=Andrew+Chelladurai
+ * Source : https://github.com/andrewchelladurai/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * OR <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ */
+
+package com.andrewchelladurai.simplebible.presenter;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.andrewchelladurai.simplebible.data.Book;
+import com.andrewchelladurai.simplebible.data.BookDao;
+import com.andrewchelladurai.simplebible.data.Bookmark;
+import com.andrewchelladurai.simplebible.data.SbDatabase;
+import com.andrewchelladurai.simplebible.data.Verse;
+import com.andrewchelladurai.simplebible.data.VerseDao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import androidx.annotation.NonNull;
+import androidx.loader.content.AsyncTaskLoader;
+
+/**
+ * Created by Andrew Chelladurai - TheUnknownAndrew[at]GMail[dot]com
+ * on 15-Sep-2018 @ 5:05 PM
+ */
+public class DbSetupLoader
+    extends AsyncTaskLoader<Boolean> {
+
+    private static final String TAG = "DbSetupLoader";
+
+    private final String FILE_VERSE_DETAILS = "init_WEB_Translation.txt";
+    private final String FILE_BOOK_DETAILS = "init_Stats.txt";
+    private final String SPLIT_REGEX = Bookmark.SEPARATOR;
+    @SuppressWarnings("FieldCanBeLocal")
+    private final int EXPECTED_BOOK_COUNT = 66;
+    @SuppressWarnings("FieldCanBeLocal")
+    private final int EXPECTED_VERSE_COUNT = 31098;
+
+    public DbSetupLoader(final Context context) {
+        super(context);
+    }
+
+    @Override
+    public Boolean loadInBackground() {
+        try {
+            final Context context = getContext();
+            final SbDatabase sbDatabase = SbDatabase.getInstance(context);
+            final BookDao bookDao = sbDatabase.getBookDao();
+            int count = bookDao.getNumberOfBooks();
+            Log.d(TAG, " [" + count + "] books exist");
+            if (count != EXPECTED_BOOK_COUNT) {
+                Log.e(TAG, " EXPECTED_BOOK_COUNT is [" + EXPECTED_BOOK_COUNT + "]");
+                bookDao.deleteAllRecords();
+                Log.d(TAG, " truncated Books table");
+                populateInitialData(sbDatabase, context, FILE_BOOK_DETAILS);
+                count = bookDao.getNumberOfBooks();
+                Log.d(TAG, " now there are [" + count + "] books");
+            }
+
+            final VerseDao verseDao = sbDatabase.getVerseDao();
+            count = verseDao.getNumberOfRecords();
+            Log.d(TAG, " [" + count + "] verses exist");
+            if (count != EXPECTED_VERSE_COUNT) {
+                Log.e(TAG, " EXPECTED_VERSE_COUNT is [" + EXPECTED_VERSE_COUNT + "]");
+                verseDao.deleteAllRecords();
+                Log.d(TAG, " truncated Verses table");
+                populateInitialData(sbDatabase, context, FILE_VERSE_DETAILS);
+                count = verseDao.getNumberOfRecords();
+                Log.d(TAG, " now there are [" + count + "] verses");
+            }
+        } catch (IOException ioe) {
+            Log.e(TAG, "database creation failed: retuning false", ioe);
+            return false;
+        }
+        return true;
+    }
+
+    private void createNewBookRecord(@NonNull final BookDao bookDao,
+                                     @NonNull final String line) {
+        final String[] parts = line.split(SPLIT_REGEX);
+        final String desc = parts[0];
+        final int number = Integer.valueOf(parts[1]);
+        final String name = parts[2];
+        final int chapters = Integer.valueOf(parts[3]);
+        final int verses = Integer.valueOf(parts[4]);
+        bookDao.createBook(new Book(desc, number, name, chapters, verses));
+    }
+
+    private void createNewVerseRecord(@NonNull final VerseDao verseDao,
+                                      @NonNull final String line) {
+        final String[] parts = line.split(SPLIT_REGEX);
+        final String translation = parts[0];
+        final int book = Integer.valueOf(parts[1]);
+        final int chapter = Integer.valueOf(parts[2]);
+        final int verse = Integer.valueOf(parts[3]);
+        final String text = parts[4];
+        verseDao.createVerse(new Verse(translation, book, chapter, verse, text));
+    }
+
+    private void populateInitialData(final @NonNull SbDatabase sbDatabase,
+                                     final @NonNull Context context,
+                                     final @NonNull String fileName)
+    throws IOException {
+        final VerseDao verseDao = sbDatabase.getVerseDao();
+        final BookDao bookDao = sbDatabase.getBookDao();
+        InputStreamReader script = getFileHandle(context, fileName);
+        Log.d(TAG, "populateInitialData: beginning execution of " + fileName);
+
+        BufferedReader reader = null;
+        boolean isCreated;
+        boolean isClosed = true;
+        try {
+            reader = new BufferedReader(script);
+            String line;
+            while (null != (line = reader.readLine())) {
+                // is this a valid line to process
+                if (!line.isEmpty() && line.contains(SPLIT_REGEX)) {
+                    switch (fileName) {
+                        case FILE_VERSE_DETAILS:
+                            createNewVerseRecord(verseDao, line);
+                            break;
+                        case FILE_BOOK_DETAILS:
+                            createNewBookRecord(bookDao, line);
+                            break;
+                        default:
+                            throw new UnsupportedOperationException(
+                                "populateInitialData: unknown filename : " + fileName);
+                    }
+                }
+            }
+            isCreated = true;
+            Log.d(TAG, "populateInitialData: finished " + fileName);
+        } catch (IOException ioe) {
+            isCreated = false;
+            Log.e(TAG, "populateInitialData: failure", ioe);
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                    isClosed = true;
+                } catch (IOException e) {
+                    isClosed = false;
+                    Log.e(TAG, "populateInitialData: failure", e);
+                }
+            }
+        }
+        String msg = (isCreated & isClosed) ? "Processed " + fileName
+                                            : "Failed executing " + fileName;
+        Log.d(TAG, "populateInitialData: " + msg);
+    }
+
+    private InputStreamReader getFileHandle(@NonNull final Context context,
+                                            @NonNull final String fileName)
+    throws IOException {
+        try {
+            return new InputStreamReader(context.getAssets().open(fileName));
+        } catch (IOException ioe) {
+            Log.e(TAG, "getFileHandle: Error opening/accessing file [" + fileName + "]", ioe);
+            throw ioe;
+        }
+    }
+}
