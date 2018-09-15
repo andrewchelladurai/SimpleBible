cd92ad5389f8d6b2804d41354590dfafb7496f37
Andrew Chelladurai <theunknownandrew@gmail.com>
2018-09-15 20:47:50 +0530

    Feat - Create Empty Main Screen (3 View Bottom Navigation Bar)

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleMainScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleMainScreen.java
@@ -1,11 +1,41 @@
+/*
+ *
+ * This file 'ActivitySimpleBible.java' is part of SimpleBible :
+ *
+ * Copyright (c) 2018.
+ *
+ * This Application is available at below locations
+ * Binary : https://play.google.com/store/apps/developer?id=Andrew+Chelladurai
+ * Source : https://github.com/andrewchelladurai/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * OR <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ */
+
 package com.andrewchelladurai.simplebible.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.presenter.DbSetupLoader;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.app.LoaderManager.LoaderCallbacks;
@@ -16,12 +46,34 @@ public class SimpleBibleMainScreen
 
     private static final String TAG = "SimpleBibleMainScreen";
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+        = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.main_bottom_navigation_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.main_bottom_navigation_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.main_bottom_navigation_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.SbTheme_Home);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple_bible);
+        setContentView(R.layout.main_screen_simple_bible);
 
         // Load database
         DbSetupLoaderCallback dbSetupLoaderCallback = new DbSetupLoaderCallback();
@@ -30,6 +82,10 @@ public class SimpleBibleMainScreen
                      .forceLoad();
 
         showLoadingScreen();
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     private void showLoadingScreen() {
