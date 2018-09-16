8d786802ba9e2cab5033be9009e0c8bacc43c800
Andrew Chelladurai <theunknownandrew@gmail.com>
2018-09-16 19:13:44 +0530

    Feat - Create HomeScreen to show daily verse

---
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/HomeScreen.java
@@ -0,0 +1,100 @@
+/*
+ *
+ * This file 'HomeScreen.java' is part of SimpleBible :
+ *
+ * Copyright (c) 2018.
+ *
+ * This Application is available at below locations
+ * Binary : https://play.google.com/store/apps/developer?id=Andrew+Chelladurai
+ * Source : https://github.com/andrewchelladurai/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * OR <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ */
+
+package com.andrewchelladurai.simplebible.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.andrewchelladurai.simplebible.R;
+import com.andrewchelladurai.simplebible.ops.HomeScreenOps;
+import com.andrewchelladurai.simplebible.presenter.HomeScreenPresenter;
+
+import androidx.core.text.HtmlCompat;
+import androidx.fragment.app.Fragment;
+
+public class HomeScreen
+    extends Fragment
+    implements HomeScreenOps {
+
+    private static final String TAG = "HomeScreen";
+
+    private static HomeScreenPresenter mPresenter;
+
+    @SuppressWarnings("WeakerAccess")
+    public HomeScreen() {
+        mPresenter = new HomeScreenPresenter(this);
+    }
+
+    /*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+*/
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_screen, container, false);
+    }
+
+    @Override
+    public void startLoadingScreen() {
+        TextView textView = getView().findViewById(R.id.daily_verse);
+        textView.setText(HtmlCompat.fromHtml(
+            getString(R.string.home_screen_daily_verse_loading),
+            HtmlCompat.FROM_HTML_MODE_LEGACY));
+    }
+
+    @Override
+    public void stopLoadingScreen() {
+        TextView textView = getView().findViewById(R.id.daily_verse);
+        textView.setText(HtmlCompat.fromHtml(
+            getString(R.string.home_screen_daily_verse_default),
+            HtmlCompat.FROM_HTML_MODE_LEGACY));
+
+        getView().findViewById(R.id.message).setVisibility(View.INVISIBLE);
+        getView().findViewById(R.id.progress_bar).setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void showFailedLoadingMessage() {
+        TextView textView = getView().findViewById(R.id.daily_verse);
+        textView.setText(HtmlCompat.fromHtml(
+            getString(R.string.home_screen_daily_verse_failure),
+            HtmlCompat.FROM_HTML_MODE_LEGACY));
+
+        textView = getView().findViewById(R.id.message);
+        textView.setText(R.string.home_screen_message_loading_failure);
+
+        getView().findViewById(R.id.progress_bar).setVisibility(View.INVISIBLE);
+    }
+
+}
