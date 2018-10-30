c5aaac8e8f66701164df1edab3c8682030e7ee9e
Andrew Chelladurai <TheUnknownAndrew@GMail.com>
2018-10-30 22:24:35 +0530

    Restart from scratch

---
+++ simplebible/src/test/java/com/andrewchelladurai/simplebible/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package com.andrewchelladurai.simplebible;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
