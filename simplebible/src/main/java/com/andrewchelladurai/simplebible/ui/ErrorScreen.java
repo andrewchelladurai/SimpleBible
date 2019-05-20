0584f57a4247739a0401038cf5c3358ccf2701c2
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-05-20 16:06:11 +0530

    Feat - Implement basic functionality of ErrorScreen
    Will accept 2 arguments (Message + Email_Dev).
    If Message is present, show it, else hide it.
    If Email_Dev = true show Button, else hide it.
    
    - TODO: Pass the message to the MainActivity to prepare a composed
    email message when the button is clicked.

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ErrorScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ErrorScreen.java
@@ -2,23 +2,22 @@ package com.andrewchelladurai.simplebible.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.andrewchelladurai.simplebible.R;
 
-
 public class ErrorScreen extends Fragment {
+    public static final String TAG = "ErrorScreen";
+    public static final String ARG_MESSAGE = "MESSAGE";
+    public static final String ARG_EMAIL_DEV = "EMAIL_DEV";
 
     private FragmentInteractionListener mListener;
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.error_screen, container, false);
-    }
+    private TextView messageView;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -35,6 +34,47 @@ public class ErrorScreen extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedState) {
+        final View view = inflater.inflate(R.layout.error_screen, container, false);
+
+        view.findViewById(R.id.err_scr_button).setOnClickListener(v -> handleClickButtonEmailDeveloper());
+        messageView = view.findViewById(R.id.err_scr_msg);
+
+        if (savedState != null) {
+            return view;
+        }
+
+        final Bundle args = getArguments();
+        if (args == null) {
+            throw new UnsupportedOperationException(TAG + " onCreateView: Null Arguments bundle passed");
+        }
+
+        if (args.containsKey(ARG_MESSAGE)
+                && args.getString(ARG_MESSAGE, null) != null
+                && !args.getString(ARG_MESSAGE, "").isEmpty()) {
+            messageView.setText(args.getString(ARG_MESSAGE, ""));
+        } else {
+            messageView.setVisibility(View.INVISIBLE);
+        }
+
+        if (args.containsKey(ARG_EMAIL_DEV)
+                && args.getBoolean(ARG_EMAIL_DEV, false)) {
+            view.findViewById(R.id.err_scr_button).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.err_scr_disclaimer).setVisibility(View.VISIBLE);
+        } else {
+            view.findViewById(R.id.err_scr_button).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.err_scr_disclaimer).setVisibility(View.INVISIBLE);
+        }
+
+        return view;
+    }
+
+    private void handleClickButtonEmailDeveloper() {
+        Log.d(TAG, "handleClickButtonEmailDeveloper: email [" + messageView.getText() + "]");
+        // TODO: 20/5/19 Pass the message to the MainActivity to prepare a composed email message
     }
 
     interface FragmentInteractionListener {
