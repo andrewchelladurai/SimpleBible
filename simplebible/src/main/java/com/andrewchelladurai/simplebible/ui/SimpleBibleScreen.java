b1b503adeff4ba87f298cd7cefd1c8c73e06bef1
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-05-22 11:49:58 +0530

    Code - MainActivity : Implement method to Create NotificationChannel

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
@@ -1,6 +1,10 @@
 package com.andrewchelladurai.simplebible.ui;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleScreenOps;
@@ -17,6 +21,21 @@ public class SimpleBibleScreen
     super.onCreate(savedState);
     setTheme(R.style.SbTheme);
     setContentView(R.layout.simple_bible_screen);
+
+    createNotificationChannel();
+  }
+
+  private void createNotificationChannel() {
+    Log.d(TAG, "createNotificationChannel:");
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel =
+          new NotificationChannel(getPackageName(),
+                                  getString(R.string.app_notification_channel_name),
+                                  NotificationManager.IMPORTANCE_HIGH);
+      channel.setDescription(getString(R.string.app_notification_channel_description));
+
+      getSystemService(NotificationManager.class).createNotificationChannel(channel);
+    }
   }
 
 }
