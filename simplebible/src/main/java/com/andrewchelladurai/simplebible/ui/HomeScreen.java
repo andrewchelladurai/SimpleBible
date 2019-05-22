06532102eba2c08af39e419d37a291b0a5dcfd65
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-05-22 11:32:34 +0530

    UIX - HomeScreen: Remove Share Fab
    We will use the BottomAppBar in the main activity for performing the ShareFab functionality.
    For this effect we obtain a reference to the main activity through "SimpleBibleScreenOps"

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/HomeScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/HomeScreen.java
@@ -5,30 +5,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import com.andrewchelladurai.simplebible.R;
+import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleScreenOps;
 
-public class HomeScreen extends Fragment {
+public class HomeScreen
+    extends Fragment {
 
-  private FragmentInteractionListener mListener;
+  private static final String TAG = "HomeScreen";
 
-  @Override public void onAttach(@NonNull Context context) {
+  private SimpleBibleScreenOps actvityOps;
+
+  private FragmentInteractionListener fragListener;
+
+  private ProgressBar pBar;
+
+  private TextView tvVerse;
+
+  @Override
+  public void onAttach(@NonNull Context context) {
     super.onAttach(context);
-    if (context instanceof FragmentInteractionListener) {
-      mListener = (FragmentInteractionListener) context;
-    } else {
-      throw new RuntimeException(context.toString() + " must implement FragmentInteractionListener");
+    if (!(context instanceof FragmentInteractionListener)) {
+      throw new RuntimeException(
+          context.toString() + " must implement FragmentInteractionListener");
     }
+    fragListener = (FragmentInteractionListener) context;
+    actvityOps = (SimpleBibleScreenOps) context;
   }
 
-  @Override public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.home_screen, container, false);
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedState) {
+    final View view = inflater.inflate(R.layout.home_screen, container, false);
+
+    pBar = view.findViewById(R.id.home_src_pbar);
+    tvVerse = view.findViewById(R.id.home_src_txt_verse);
+    tvVerse.setText(HtmlCompat.fromHtml(getString(R.string.home_src_txt_verse_default),
+                                        HtmlCompat.FROM_HTML_MODE_LEGACY));
+
+    return view;
   }
 
-  @Override public void onDetach() {
+  @Override
+  public void onDetach() {
     super.onDetach();
-    mListener = null;
+    fragListener = null;
+    actvityOps = null;
   }
 
   interface FragmentInteractionListener {
