327c76715f8df13c1560453720ee38f68afcfed7
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-05-22 16:38:42 +0530

    Feat - MainScreen : Create bottomAppBar menu and implement it
    Also put the NavHostFragment in a NestedScrollView

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
@@ -6,13 +6,21 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
 import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleScreenOps;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class SimpleBibleScreen
     extends AppCompatActivity
     implements SimpleBibleScreenOps,
-               HomeScreen.FragmentInteractionListener {
+               HomeScreen.FragmentInteractionListener,
+               BookListScreen.FragmentInteractionListener,
+               SearchScreen.FragmentInteractionListener,
+               BookmarkListScreen.OnFragmentInteractionListener,
+               PreferencesScreen.FragmentInteractionListener {
 
   private static final String TAG = "SimpleBibleScreen";
 
@@ -25,6 +33,56 @@ public class SimpleBibleScreen
     setContentView(R.layout.simple_bible_screen);
 
     createNotificationChannel();
+
+    setupNavigationComponent();
+  }
+
+  private void setupNavigationComponent() {
+    Log.d(TAG, "setupNavigationComponent:");
+
+    final NavController navController =
+        Navigation.findNavController(this, R.id.main_scr_nav_host_fragment);
+    final NavDestination currentDestination = navController.getCurrentDestination();
+    final BottomAppBar appBar = findViewById(R.id.main_scr_bottom_app_bar);
+
+    appBar.setOnMenuItemClickListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.homeScreen:
+          if (currentDestination != null
+              && currentDestination.getId() != R.id.homeScreen) {
+            navController.navigate(R.id.action_global_homeScreen);
+          }
+          return true;
+        case R.id.bookListScreen:
+          if (currentDestination != null
+              && currentDestination.getId() != R.id.bookListScreen) {
+            navController.navigate(R.id.action_global_bookListScreen);
+          }
+          return true;
+        case R.id.searchScreen:
+          if (currentDestination != null
+              && currentDestination.getId() != R.id.searchScreen) {
+            navController.navigate(R.id.action_global_searchScreen);
+          }
+          return true;
+        case R.id.bookmarkListScreen:
+          if (currentDestination != null
+              && currentDestination.getId() != R.id.bookmarkListScreen) {
+            navController.navigate(R.id.action_global_bookmarkListScreen);
+          }
+          return true;
+        case R.id.preferencesScreen:
+          if (currentDestination != null
+              && currentDestination.getId() != R.id.preferencesScreen) {
+            navController.navigate(R.id.action_global_preferencesScreen);
+          }
+          return true;
+        default:
+          Log.e(TAG, "setupNavigationComponent: unknown MenuItem["
+                     + item.getTitle() + "-" + item.getItemId() + "]");
+          return false;
+      }
+    });
   }
 
   private void createNotificationChannel() {
