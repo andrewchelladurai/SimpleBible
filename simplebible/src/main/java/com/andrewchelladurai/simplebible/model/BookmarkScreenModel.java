544d194c727feb6b018ea8c361a2f409bc4efff9
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-06-01 16:52:38 +0530

    Code:BookmarkScreen:Restart implementing functionality

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/model/BookmarkScreenModel.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/model/BookmarkScreenModel.java
@@ -1,26 +1,14 @@
 package com.andrewchelladurai.simplebible.model;
 
 import android.app.Application;
-import android.util.Log;
 import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import com.andrewchelladurai.simplebible.data.SbDatabase;
-import com.andrewchelladurai.simplebible.data.dao.BookDao;
 import com.andrewchelladurai.simplebible.data.dao.BookmarkDao;
 import com.andrewchelladurai.simplebible.data.entities.Book;
-import com.andrewchelladurai.simplebible.data.entities.Bookmark;
-import com.andrewchelladurai.simplebible.data.entities.Verse;
 import com.andrewchelladurai.simplebible.utils.BookUtils;
-import com.andrewchelladurai.simplebible.utils.BookmarkUtils;
-import com.andrewchelladurai.simplebible.utils.BookmarkUtils.CreateBookmarkTask;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class BookmarkScreenModel
     extends AndroidViewModel {
@@ -28,65 +16,17 @@ public class BookmarkScreenModel
   private static final String TAG = "BookmarkScreenModel";
 
   private final BookmarkDao bookmarkDao;
-  private final BookDao bookDao;
-  private final ArrayList<Verse> list = new ArrayList<Verse>();
 
   public BookmarkScreenModel(@NonNull final Application application) {
     super(application);
-    bookDao = SbDatabase.getDatabase(getApplication()).getBookDao();
     bookmarkDao = SbDatabase.getDatabase(getApplication()).getBookmarkDao();
-  }
-
-  public void updateCacheList(final List<?> newList) {
-    list.clear();
-    for (final Object o : newList) {
-      list.add((Verse) o);
-    }
-
-    //noinspection unchecked
-    Collections.sort(list);
-  }
-
-  public List<?> getCachedList() {
-    return list;
-  }
-
-  public Verse getCachedItemAt(final int position) {
-    return list.get(position);
-  }
-
-  public int getCachedListSize() {
-    return list.size();
   }
 
   public LiveData<Book> getBook(
       @IntRange(from = 1, to = BookUtils.EXPECTED_COUNT) final int bookNumber) {
-    return bookDao.getRecordLive(bookNumber);
-  }
-
-  public String createBookmarkReference(@NonNull final List<?> newList) {
-    return BookmarkUtils.createBookmarkReference(newList);
-  }
-
-  @NonNull
-  public LiveData<Integer> doesBookmarkExist(final String reference) {
-    return bookDao.doesBookmarkExist(reference);
-  }
-
-  @Nullable
-  public Integer createBookmark(@NonNull final String note) {
-    final String reference = BookmarkUtils.createBookmarkReference(list);
-    Log.d(TAG, "createBookmark: note = [" + note + "], reference[" + reference + "]");
-    final CreateBookmarkTask createBookmarkTask = new CreateBookmarkTask(bookmarkDao);
-    createBookmarkTask.execute(new Bookmark(reference, note));
-    try {
-      return createBookmarkTask.get();
-    } catch (ExecutionException e) {
-      Log.e(TAG, "createBookmark: ", e);
-    } catch (InterruptedException e) {
-      Log.e(TAG, "createBookmark: ", e);
-    }
-    return null;
+    return SbDatabase.getDatabase(getApplication())
+                     .getBookDao()
+                     .getRecordLive(bookNumber);
   }
 
 }
--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/BookmarkScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/BookmarkScreen.java
@@ -3,13 +3,11 @@ package com.andrewchelladurai.simplebible.ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
@@ -24,6 +22,7 @@ import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleScreenOps;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -62,24 +61,6 @@ public class BookmarkScreen
                            Bundle savedState) {
     final View view = inflater.inflate(R.layout.bookmark_screen, container, false);
 
-    final Bundle args = getArguments();
-    if (args == null) {
-      final String message = getString(R.string.bookmark_scr_err_no_args_passed);
-      activityOps.showErrorScreen(message, true);
-    } else if (!args.containsKey(ARG_ARRAY_VERSES)) {
-      final String message = getString(R.string.bookmark_scr_err_no_verses_passed);
-      activityOps.showErrorScreen(message, true);
-    } else {
-      final Parcelable[] array = args.getParcelableArray(ARG_ARRAY_VERSES);
-      if (array == null || array.length == 0) {
-        final String message = getString(R.string.bookmark_scr_err_empty_verse_array_passed);
-        activityOps.showErrorScreen(message, true);
-      } else {
-        contentTemplate = getString(R.string.item_bookmark_verse_content_template);
-        verseListAdapter.refreshList(Arrays.asList(array));
-      }
-    }
-
     editButton = view.findViewById(R.id.bookmark_scr_action_edit);
     editButton.setOnClickListener(v -> handleClickActionEdit());
 
@@ -99,6 +80,25 @@ public class BookmarkScreen
 
     activityOps.hideNavigationComponent();
 
+    final Bundle args = getArguments();
+    if (args == null) {
+      final String message = getString(R.string.bookmark_scr_err_no_args_passed);
+      activityOps.showErrorScreen(message, true);
+    } else if (!args.containsKey(ARG_ARRAY_VERSES)) {
+      final String message = getString(R.string.bookmark_scr_err_no_verses_passed);
+      activityOps.showErrorScreen(message, true);
+    } else {
+      final Parcelable[] array = args.getParcelableArray(ARG_ARRAY_VERSES);
+      if (array == null || array.length == 0) {
+        final String message = getString(R.string.bookmark_scr_err_empty_verse_array_passed);
+        activityOps.showErrorScreen(message, true);
+      } else {
+        // now we have the array of verses passed to this screen
+        contentTemplate = getString(R.string.item_bookmark_verse_content_template);
+        verseListAdapter.refreshList(Arrays.asList(array));
+      }
+    }
+
     return view;
   }
 
@@ -115,19 +115,11 @@ public class BookmarkScreen
   }
 
   private void handleClickActionSave() {
-    final Editable fieldText = noteField.getText();
-    final String note = (fieldText != null) ? fieldText.toString().trim() : "";
-    final Integer bookmarkRow = model.createBookmark(note);
-    final String message = (bookmarkRow != null)
-                           ? getString(R.string.bookmark_scr_new_record_created)
-                           : getString(R.string.bookmark_scr_new_record_failed);
-    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
-    toggleActionButtons(bookmarkRow != null);
+    // TODO: 1/6/19 implement this
   }
 
   private void handleClickActionEdit() {
-    noteField.setEnabled(true);
-    noteField.requestFocus();
+    // TODO: 1/6/19 implement this
   }
 
   private void handleClickActionShare() {
@@ -136,32 +128,27 @@ public class BookmarkScreen
 
   @Override
   public void refreshList(@NonNull final List<?> newList) {
-    model.doesBookmarkExist(model.createBookmarkReference(newList))
-         .observe(this, count -> {
-           final boolean exists = (count > 0);
-           Log.d(TAG, "refreshList: exists [" + exists + "]");
-
-           toggleActionButtons(exists);
-
-           model.updateCacheList(newList);
-         });
+    // TODO: 1/6/19 implement this
   }
 
   @NonNull
   @Override
   public List<?> getCachedList() {
-    return model.getCachedList();
+    // TODO: 1/6/19 implement this
+    return new ArrayList<>();
   }
 
   @NonNull
   @Override
   public Object getCachedItemAt(final int position) {
-    return model.getCachedItemAt(position);
+    // TODO: 1/6/19 implement this
+    return new Object();
   }
 
   @Override
   public int getCachedListSize() {
-    return model.getCachedListSize();
+    // TODO: 1/6/19 implement this
+    return 0;
   }
 
   @Override
@@ -178,19 +165,6 @@ public class BookmarkScreen
                                                          verse.getText()),
                                            HtmlCompat.FROM_HTML_MODE_LEGACY));
     });
-  }
-
-  private void toggleActionButtons(final boolean bookmarkExists) {
-    // if a bookmark doesn't exist, the only valid option to show is to save it
-    saveButton.setVisibility((bookmarkExists) ? View.GONE : View.VISIBLE);
-
-    // if a bookmark exists, these are the valid options
-    editButton.setVisibility((bookmarkExists) ? View.VISIBLE : View.GONE);
-    deleteButton.setVisibility((bookmarkExists) ? View.VISIBLE : View.GONE);
-    shareButton.setVisibility((bookmarkExists) ? View.VISIBLE : View.GONE);
-
-    // update the note field to be enabled or disabled if the bookmark exists
-    noteField.setEnabled(!bookmarkExists);
   }
 
 }
