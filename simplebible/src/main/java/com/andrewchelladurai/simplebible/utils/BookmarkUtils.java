42af661cff8ae344a5ec30ae1ff200330ae13770
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-06-02 17:29:19 +0530

    Fix- Use Loader instead of a task

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/utils/BookmarkUtils.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/utils/BookmarkUtils.java
@@ -1,12 +1,14 @@
 package com.andrewchelladurai.simplebible.utils;
 
-import android.os.AsyncTask;
+import android.content.Context;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+import com.andrewchelladurai.simplebible.data.SbDatabase;
 import com.andrewchelladurai.simplebible.data.dao.BookmarkDao;
 import com.andrewchelladurai.simplebible.data.entities.Bookmark;
 import com.andrewchelladurai.simplebible.data.entities.Verse;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -42,22 +44,27 @@ public class BookmarkUtils {
     return builder.substring(0, builder.length() - SEPARATOR.length());
   }
 
-  public static class CreateBookmarkTask
-      extends AsyncTask<Bookmark, Void, Void> {
+  public static class CreateBookmarkLoader
+      extends AsyncTaskLoader<Boolean> {
 
-    private static final String TAG = "CreateBookmarkTask";
-    private BookmarkDao bookmarkDao;
+    public static final int ID = 13;
+    private static final String TAG = "CreateBookmarkLoader";
+    private final Bookmark bookmark;
+    private final BookmarkDao bookmarkDao;
 
-    public CreateBookmarkTask(@NonNull final BookmarkDao bookmarkDao) {
-      this.bookmarkDao = bookmarkDao;
+    public CreateBookmarkLoader(@NonNull final Context context,
+                                final Bookmark bookmark) {
+      super(context);
+      bookmarkDao = SbDatabase.getDatabase(getContext()).getBookmarkDao();
+      this.bookmark = bookmark;
     }
 
+    @Nullable
     @Override
-    protected Void doInBackground(final Bookmark... bookmarks) {
-      final Bookmark bookmark = bookmarks[0];
-      Log.d(TAG, "doInBackground() called with: bookmarks = [" + bookmark + "]");
+    public Boolean loadInBackground() {
+      Log.d(TAG, "loadInBackground: " + bookmark);
       bookmarkDao.createRecord(bookmark);
-      return null;
+      return true;
     }
 
   }
