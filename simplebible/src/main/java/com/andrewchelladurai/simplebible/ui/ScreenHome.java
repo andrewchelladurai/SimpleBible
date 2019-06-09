d8a5a73e0b99362a4495e1d04f6d6d71fa65f88b
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-06-10 02:09:17 +0530

    Implement basic view logic depending on previous saved state.
    Create placeholder methods to
    handleActionShare()
    startDbSetupService()

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ScreenHome.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ScreenHome.java
@@ -2,16 +2,24 @@ package com.andrewchelladurai.simplebible.ui;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.ui.ops.ScreenSimpleBibleOps;
 
+import static androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY;
+
 public class ScreenHome
     extends Fragment {
+
+  private static final String TAG = "ScreenHome";
 
   private ScreenSimpleBibleOps mainOps;
   private View rootView;
@@ -32,6 +40,24 @@ public class ScreenHome
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedState) {
     rootView = inflater.inflate(R.layout.screen_home_fragment, container, false);
+
+    // setup listener for the share fab
+    rootView.findViewById(R.id.scrHomeFabShare)
+            .setOnClickListener(v -> handleActionShare());
+
+    if (savedState == null) {
+      // show the verse text for a loading progress
+      showVerseText(R.string.scrHomeVerseLoading);
+
+      // show the progress bar
+      rootView.findViewById(R.id.scrHomeProgress).setVisibility(View.VISIBLE);
+
+      // hide the share fab
+      rootView.findViewById(R.id.scrHomeFabShare).setVisibility(View.GONE);
+
+      startDbSetupService();
+    }
+
     return rootView;
   }
 
@@ -39,6 +65,20 @@ public class ScreenHome
   public void onDetach() {
     super.onDetach();
     mainOps = null;
+  }
+
+  private void startDbSetupService() {
+    Log.d(TAG, "startDbSetupService:");
+  }
+
+  private void showVerseText(@StringRes int stringResId) {
+    ((TextView) rootView.findViewById(R.id.scrHomeVerse))
+        .setText(HtmlCompat.fromHtml(getString(stringResId), FROM_HTML_MODE_LEGACY));
+  }
+
+  private void handleActionShare() {
+    Log.d(TAG, "handleActionShare:");
+    // TODO: 10/6/19 share the text in the verse view
   }
 
 }
