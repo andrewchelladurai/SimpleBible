f8ef209e5d5b963ce128c3b36585403f741d2a74
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-06-15 23:53:50 +0530

    Create methods needed during database setup

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/BookDao.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/BookDao.java
@@ -1,8 +1,27 @@
 package com.andrewchelladurai.simplebible.data.dao;
 
+import androidx.annotation.IntRange;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.andrewchelladurai.simplebible.data.entity.Book;
+import com.andrewchelladurai.simplebible.utils.BookUtils;
 
 @Dao
 public interface BookDao {
+
+  @IntRange(from = 1, to = BookUtils.EXPECTED_COUNT)
+  @Query("select count(number) from sb_books;")
+  int getBookCount();
+
+  @Nullable
+  @Query("select * from sb_books where number=:bookNumber;")
+  Book getBookUsingPosition(@IntRange(from = 1, to = BookUtils.EXPECTED_COUNT) int bookNumber);
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  void createBook(@NonNull Book book);
 
 }
--- simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/data/dao/VerseDao.java
@@ -1,8 +1,23 @@
 package com.andrewchelladurai.simplebible.data.dao;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.andrewchelladurai.simplebible.data.entity.Verse;
 
 @Dao
 public interface VerseDao {
+
+  @Query("select count(book||chapter||verse) from sb_verses;")
+  int getVerseCount();
+
+  @Query("select count(book||chapter||verse) from sb_verses;")
+  LiveData<Integer> getLiveVerseCount();
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  void createVerse(@NonNull Verse verse);
 
 }
