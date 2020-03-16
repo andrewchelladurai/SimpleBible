508c1c1d2d13bb70453739262f01a520d828917d
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-03-16 22:46:21 +0530

    REF - Code formatting / Beautifier changes.

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SimpleBibleScreen.java
@@ -1,49 +1,38 @@
 package com.andrewchelladurai.simplebible.ui;
 
-
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.andrewchelladurai.simplebible.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class SimpleBibleScreen
-        extends AppCompatActivity {
+    extends AppCompatActivity {
 
-    private TextView mTextMessage;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.simple_bible_screen);
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_bible_screen);
-
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+    TextView textView = findViewById(R.id.message);
+    BottomNavigationView navigation = findViewById(R.id.navigation);
+    navigation.setOnNavigationItemSelectedListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.navigation_home:
+          textView.setText(R.string.title_home);
+          return true;
+        case R.id.navigation_dashboard:
+          textView.setText(R.string.title_dashboard);
+          return true;
+        case R.id.navigation_notifications:
+          textView.setText(R.string.title_notifications);
+          return true;
+        default:
+      }
+      return false;
+    });
+  }
 
 }
