46eb95e05135aeca6e6ee812ea0726311b513400
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-03-17 03:01:47 +0530

    On Screen load, update Theme summary to show current selection

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SettingsScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SettingsScreen.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.andrewchelladurai.simplebible.R;
@@ -19,8 +20,37 @@ public class SettingsScreen
 
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-    setPreferencesFromResource(R.xml.root_preferences, rootKey);
     Log.d(TAG, "onCreatePreferences:");
+    setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+    updateSectionTheme();
+  }
+
+  private void updateSectionTheme() {
+    Log.d(TAG, "updateSectionTheme:");
+    final String key = getString(R.string.pref_theme_key);
+
+    final Preference preference = findPreference(key);
+    if (preference == null) {
+      Log.e(TAG, "updateSectionTheme: ",
+            new NullPointerException("No preference found for key[" + key + "]"));
+      return;
+    }
+
+    // get the value set for the preference
+    final String value = getPreferenceManager()
+                             .getSharedPreferences()
+                             .getString(key, getString(R.string.pref_theme_value_system));
+
+    // depending on the current value, update the summary so the user can know at a glance
+    if (value.equalsIgnoreCase(getString(R.string.pref_theme_value_yes))) {
+      preference.setSummary(R.string.pref_theme_entry_yes);
+    } else if (value.equalsIgnoreCase(getString(R.string.pref_theme_value_no))) {
+      preference.setSummary(R.string.pref_theme_entry_no);
+    } else {
+      preference.setSummary(R.string.pref_theme_entry_system);
+    }
+
   }
 
   @Override public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
