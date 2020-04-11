4978ba6802d97d0a4b793918c0a0f1e42a098b3b
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-04-11 12:47:03 +0530

    Setup listeners for actions on the SearchScreen UI

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SearchScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SearchScreen.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +21,7 @@ import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.model.SearchViewModel;
 import com.andrewchelladurai.simplebible.ui.ops.SearchScreenOps;
 import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleOps;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class SearchScreen
     extends Fragment
@@ -53,6 +57,47 @@ public class SearchScreen
     Log.d(TAG, "onCreateView:");
     rootView = inflater.inflate(R.layout.search_screen, container, false);
 
+    final BottomAppBar appBar = rootView.findViewById(R.id.bottom_app_bar_scr_search);
+    appBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+
+      @Override
+      public boolean onMenuItemClick(final MenuItem item) {
+        switch (item.getItemId()) {
+          case R.id.action_menu_scr_search_clear:
+            handleActionClear();
+            return true;
+          case R.id.action_menu_scr_search_bookmark:
+            handleActionBookmark();
+            return true;
+          case R.id.action_menu_scr_search_share:
+            handleActionShare();
+            return true;
+          case R.id.action_menu_scr_search_search:
+            final SearchView searchView = rootView.findViewById(R.id.search_view_scr_search);
+            handleActionSearch(searchView.getQuery().toString());
+            return true;
+          default:
+            Log.d(TAG, "onMenuItemClick: unknown menu item " + item.getTitle());
+            return false;
+        }
+      }
+    });
+
+    final SearchView searchView = rootView.findViewById(R.id.search_view_scr_search);
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+      @Override
+      public boolean onQueryTextSubmit(final String query) {
+        handleActionSearch((query == null) ? "" : query);
+        return true;
+      }
+
+      @Override
+      public boolean onQueryTextChange(final String newText) {
+        return false;
+      }
+    });
+
     if (savedInstanceState == null) {
       showHelpText();
     }
@@ -63,7 +108,8 @@ public class SearchScreen
   private void showHelpText() {
     Log.d(TAG, "showHelpText:");
     rootView.findViewById(R.id.list_view_scr_search).setVisibility(View.GONE);
-    rootView.findViewById(R.id.contain_bottom_app_bar_scr_search).setVisibility(View.GONE);
+
+    setVerseSelectionActionsVisibility(false);
 
     ((TextView) rootView.findViewById(R.id.help_text_scr_search))
         .setText(HtmlCompat.fromHtml(getString(R.string.help_text_scr_search),
@@ -74,9 +120,35 @@ public class SearchScreen
   private void showSearchResults() {
     Log.d(TAG, "showSearchResults:");
     rootView.findViewById(R.id.list_view_scr_search).setVisibility(View.VISIBLE);
-    rootView.findViewById(R.id.contain_bottom_app_bar_scr_search).setVisibility(View.VISIBLE);
+
+    setVerseSelectionActionsVisibility(false);
 
     rootView.findViewById(R.id.contain_help_text_scr_search).setVisibility(View.GONE);
+  }
+
+  private void setVerseSelectionActionsVisibility(final boolean visible) {
+    final BottomAppBar appBar = rootView.findViewById(R.id.bottom_app_bar_scr_search);
+    appBar.getMenu().setGroupVisible(R.id.menu_group_selection_scr_search, visible);
+  }
+
+  private void handleActionShare() {
+    Log.d(TAG, "handleActionShare:");
+  }
+
+  private void handleActionBookmark() {
+    Log.d(TAG, "handleActionBookmark:");
+  }
+
+  private void handleActionClear() {
+    Log.d(TAG, "handleActionClear:");
+  }
+
+  private void handleActionSearch(@NonNull final String text) {
+    Log.d(TAG, "handleActionSearch: text = [" + text + "]");
+    if (text.isEmpty()) {
+      showHelpText();
+      return;
+    }
   }
 
 }
