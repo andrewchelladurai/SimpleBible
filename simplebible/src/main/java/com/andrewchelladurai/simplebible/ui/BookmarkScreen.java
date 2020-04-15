d01e59d27729dbabc4c3823aac53f67f94683f3b
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-04-15 22:35:49 +0530

    UIX - Create components of BookmarkScreen

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/BookmarkScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/BookmarkScreen.java
@@ -16,6 +16,7 @@ import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.model.BookmarkViewModel;
 import com.andrewchelladurai.simplebible.ui.ops.BookmarkScreenOps;
 import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleOps;
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class BookmarkScreen
     extends Fragment
@@ -23,11 +24,13 @@ public class BookmarkScreen
 
   private static final String TAG = "BookmarkScreen";
 
-  public static final String ARG_STR_REFERENCE = "ARG_STR_REFERENCE";
+  static final String ARG_STR_REFERENCE = "ARG_STR_REFERENCE";
 
   private BookmarkViewModel model;
 
   private SimpleBibleOps ops;
+
+  private View rootView;
 
   @Override
   public void onAttach(@NonNull final Context context) {
@@ -49,7 +52,49 @@ public class BookmarkScreen
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                            @Nullable Bundle savedInstanceState) {
     Log.d(TAG, "onCreateView:");
-    return inflater.inflate(R.layout.bookmark_detail_screen, container, false);
+    ops.hideKeyboard();
+    ops.hideNavigationView();
+
+    rootView = inflater.inflate(R.layout.bookmark_detail_screen, container, false);
+
+    ((BottomAppBar) rootView.findViewById(R.id.scr_bookmark_details_app_bar))
+        .setOnMenuItemClickListener(item -> {
+          switch (item.getItemId()) {
+            case R.id.menu_action_save_scr_bookmark_detail:
+              handleActionSave();
+              return true;
+            case R.id.menu_action_delete_scr_bookmark_detail:
+              handleActionDelete();
+              return true;
+            case R.id.menu_action_edit_scr_bookmark_detail:
+              handleActionEdit();
+              return true;
+            case R.id.menu_action_share_scr_bookmark_detail:
+              handleActionShare();
+              return true;
+            default:
+              Log.e(TAG, "onCreateView: unknown Menu item [" + item.getTitle() + "] captured");
+              return false;
+          }
+        });
+
+    return rootView;
+  }
+
+  private void handleActionSave() {
+    Log.d(TAG, "handleActionSave:");
+  }
+
+  private void handleActionDelete() {
+    Log.d(TAG, "handleActionDelete:");
+  }
+
+  private void handleActionEdit() {
+    Log.d(TAG, "handleActionEdit:");
+  }
+
+  private void handleActionShare() {
+    Log.d(TAG, "handleActionShare:");
   }
 
 }
