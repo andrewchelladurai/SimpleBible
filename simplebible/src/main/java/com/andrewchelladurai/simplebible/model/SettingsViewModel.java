53f50e7e5b657a5f59907faaf0cb4e94939957fd
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-05-01 21:37:02 +0530

    Return default Reminder Time value of 08:00 Hrs if validation fails
    Remove unnecessary comments
    Throw Exceptions when a validation fails with the reason so that it's
    captured in logs.

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/model/SettingsViewModel.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/model/SettingsViewModel.java
@@ -20,21 +20,21 @@ public class SettingsViewModel
   }
 
   public int[] splitReminderKeyValue(@NonNull final String value) {
-    Log.d(TAG, "getReminderKeyValue() called with: value = [" + value + "]");
     if (validateReminderKeyValue(value)) {
       final String[] parts = value.split(KEY_VALUE_SEPARATOR);
       return new int[]{Integer.parseInt(parts[0]), Integer.parseInt(parts[1])};
     }
-    throw new IllegalArgumentException("argument value[" + value + "] failed validation");
+    Log.e(TAG, "splitReminderKeyValue: ",
+          new IllegalArgumentException("value[" + value + "] failed validation, returning "
+                                       + "default HH:MM value 08:00"));
+    return new int[]{8, 0};
   }
 
   public boolean validateReminderKeyValue(@NonNull final String value) {
-    Log.d(TAG, "validateReminderKeyValue: value[" + value + "]");
-
     final String[] parts = value.split(KEY_VALUE_SEPARATOR);
     if (parts.length != 2) {
-      Log.e(TAG, "validateReminderKeyValue: we expected 2 parts [HH~MM] in the value "
-                 + "and found [" + parts.length + "] parts in it.");
+      Log.e(TAG, "validateReminderKeyValue:", new IllegalArgumentException(
+          "value expected to have 2 parts [HH~MM] but it has [" + parts.length + "]"));
       return false;
     }
 
@@ -47,8 +47,8 @@ public class SettingsViewModel
     }
 
     if (intValue < 0 || intValue > 23) {
-      Log.e(TAG, "validateReminderKeyValue: hour[" + intValue
-                 + "] is not within expected limit [0...23]");
+      Log.e(TAG, "validateReminderKeyValue:", new IllegalArgumentException(
+          "invalid hour value [" + intValue + "] must be between [0...23]"));
       return false;
     }
 
@@ -60,8 +60,8 @@ public class SettingsViewModel
     }
 
     if (intValue < 0 || intValue > 59) {
-      Log.e(TAG, "validateReminderKeyValue: minute[" + intValue
-                 + "] is not within expected limit [0...59]");
+      Log.e(TAG, "validateReminderKeyValue:", new IllegalArgumentException(
+          "invalid minute value [" + intValue + "] must be between [0...59]"));
       return false;
     }
 
