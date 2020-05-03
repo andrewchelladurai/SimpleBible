84d0d919190550ba2e0edc65d9309ab565454cfa
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-05-03 14:15:08 +0530

    SettingScreen - Restart logic implementation from scratch
    Also correct the summary text shown for the DarkTheme preference.
    Since the default selection is AUTO, the summary text will reflect it.

--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SettingsScreen.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/SettingsScreen.java
@@ -1,25 +1,15 @@
 package com.andrewchelladurai.simplebible.ui;
 
-import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -28,8 +18,6 @@ import androidx.preference.PreferenceManager;
 import com.andrewchelladurai.simplebible.R;
 import com.andrewchelladurai.simplebible.model.SettingsViewModel;
 import com.andrewchelladurai.simplebible.ui.ops.SimpleBibleOps;
-
-import java.util.Calendar;
 
 public class SettingsScreen
     extends PreferenceFragmentCompat {
@@ -78,73 +66,20 @@ public class SettingsScreen
       prefClickListener = new ClickListener();
     }
 
-    getPreferenceManager().getSharedPreferences()
-                          .registerOnSharedPreferenceChangeListener(prefChangeHandler);
-    getPreferenceManager().setOnPreferenceTreeClickListener(prefClickListener);
+    final PreferenceManager prefManager = getPreferenceManager();
+    prefManager.getSharedPreferences()
+               .registerOnSharedPreferenceChangeListener(prefChangeHandler);
+    prefManager.setOnPreferenceTreeClickListener(prefClickListener);
   }
 
   @Override
   public void onPause() {
     super.onPause();
 
-    getPreferenceManager().getSharedPreferences()
-                          .unregisterOnSharedPreferenceChangeListener(prefChangeHandler);
-    getPreferenceManager().setOnPreferenceTreeClickListener(null);
-  }
-
-  @Override
-  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-    Log.d(TAG, "onCreatePreferences:");
-    setPreferencesFromResource(R.xml.root_preferences, rootKey);
-
-    updateSectionTheme();
-    updateSectionReminderTime();
-  }
-
-  private void updateSectionTheme() {
-    final String key = getString(R.string.pref_theme_key);
-    Log.d(TAG, "updateSectionTheme:key[" + key + "]");
-
-    final SharedPreferences sPreferences = getPreferenceManager().getSharedPreferences();
-    final Preference pref = getPreferenceScreen().findPreference(key);
-
-    if (pref != null) {
-      final String value =
-          sPreferences.getString(key, getString(R.string.pref_theme_value_system));
-      if (value.equalsIgnoreCase(getString(R.string.pref_theme_value_yes))) {
-        pref.setSummary(getString(R.string.pref_theme_entry_yes));
-      } else if (value.equalsIgnoreCase(getString(R.string.pref_theme_value_no))) {
-        pref.setSummary(getString(R.string.pref_theme_entry_no));
-      } else {
-        pref.setSummary(getString(R.string.pref_theme_entry_system));
-      }
-    } else {
-      Log.e(TAG, "updateSectionTheme: could not locate preference for key [" + key + "]");
-    }
-  }
-
-  private void updateSectionReminderTime() {
-    final String key = getString(R.string.pref_reminder_time_key);
-    Log.d(TAG, "updateSectionReminderTime: populating section for [" + key + "]");
-
-    final SharedPreferences preferences = getPreferenceManager().getSharedPreferences();
-    final Preference pref = getPreferenceScreen().findPreference(key);
-
-    if (pref != null) {
-      final String value = preferences.getString(key, "");
-
-      if (model.validateReminderKeyValue(value)) {
-        final int[] parts = model.splitReminderKeyValue(value);
-        final Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, parts[0]);
-        cal.set(Calendar.MINUTE, parts[1]);
-        pref.setSummary(DateFormat.getTimeFormat(requireActivity()).format(cal.getTime()));
-      } else {
-        pref.setSummary(getString(R.string.pref_reminder_time_summary_invalid));
-      }
-    } else {
-      Log.d(TAG, "updateSectionReminderTime: could not locate preference for key [" + key + "]");
-    }
+    final PreferenceManager prefManager = getPreferenceManager();
+    prefManager.getSharedPreferences()
+               .unregisterOnSharedPreferenceChangeListener(prefChangeHandler);
+    prefManager.setOnPreferenceTreeClickListener(null);
   }
 
   @Override
@@ -156,68 +91,37 @@ public class SettingsScreen
     return super.onCreateView(inflater, container, savedInstanceState);
   }
 
-  private void handlePreferenceClickTheme() {
-    Log.d(TAG, "handlePreferenceClickTheme:");
+  @Override
+  public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    Log.d(TAG, "onCreatePreferences:");
+    setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+    updateSummaryTheme(getString(R.string.pref_theme_key));
+    updateSummaryReminder(getString(R.string.pref_reminder_key));
   }
 
-  private void handlePreferenceClickExport() {
-    Log.d(TAG, "handlePreferenceClickExport:");
+  private void updateSummaryTheme(final String key) {
+    Log.d(TAG, "updateSummaryTheme: key[" + key + "]");
   }
 
-  private void handlePreferenceClickReminder() {
-    Log.d(TAG, "handlePreferenceClickReminder:");
-    final SharedPreferences preferences = getPreferenceManager().getSharedPreferences();
-
-    final String key = getString(R.string.pref_reminder_time_key);
-    final String value = preferences.getString(key, "");
-    final int[] hourMinute = new int[2];
-
-    if (model.validateReminderKeyValue(value)) {
-      final int[] array = model.splitReminderKeyValue(value);
-      hourMinute[0] = array[0];
-      hourMinute[1] = array[1];
-    } else {
-      final Calendar now = Calendar.getInstance();
-      hourMinute[0] = now.get(Calendar.HOUR_OF_DAY);
-      hourMinute[1] = now.get(Calendar.MINUTE);
-    }
-
-    final FragmentActivity fActivity = requireActivity();
-    new TimePickerDialog(
-        fActivity,
-        (view, hour, min) -> {
-          updateKeyValueReminderTime(hour, min);
-          updateNotificationReminder();
-        },
-        hourMinute[0],
-        hourMinute[1],
-        DateFormat.is24HourFormat(fActivity)
-    ).show();
+  private void updateSummaryReminder(@NonNull final String key) {
+    Log.d(TAG, "updateSummaryReminder: key[" + key + "]");
   }
 
-  private void updateKeyValueReminderTime(@IntRange(from = 0, to = 23) final int hour,
-                                          @IntRange(from = 0, to = 59) final int min) {
-    Log.d(TAG, "updateKeyValueReminderTime: hour = [" + hour + "], min = [" + min + "]");
+  private void handleValueChangeTheme(final String key) {
+    Log.d(TAG, "handleValueChangeTheme: key[" + key + "]");
+    updateSummaryTheme(key);
 
-    // save the HH:MM values passed in the preferences
-    final SharedPreferences.Editor editor = getPreferenceManager().getSharedPreferences().edit();
-    final String key = getString(R.string.pref_reminder_time_key);
-    editor.putString(key, model.createReminderKeyValue(hour, min));
-    editor.apply();
-
-    final Preference pref = getPreferenceScreen().findPreference(key);
-    if (pref == null) {
-      Log.e(TAG, "updateKeyValueReminderTime: no pref found  with key[" + key + "]");
-      return;
-    }
-
-    // format the HH:MM values to show in the summary text of the preference entry
-    final Calendar cal = Calendar.getInstance();
-    cal.set(Calendar.HOUR_OF_DAY, hour);
-    cal.set(Calendar.MINUTE, min);
-    pref.setSummary(DateFormat.getTimeFormat(requireActivity()).format(cal.getTime()));
+    ops.applyThemeSelectedInPreference();
+    ops.restartApp();
   }
 
+  private void handleValueChangeReminder(final String key) {
+    Log.d(TAG, "handleValueChangeReminder: key[" + key + "]");
+    updateSummaryReminder(key);
+  }
+
+/*
   private void handlePreferenceClickRate() {
     Log.d(TAG, "handlePreferenceClickRate:");
     final String packName = requireActivity().getPackageName();
@@ -287,23 +191,7 @@ public class SettingsScreen
     showAlertWebView("licenses.html",
                      R.string.scr_settings_err_not_found_license);
   }
-
-  private void updateNotificationReminder() {
-    Log.d(TAG, "updateNotificationReminder:");
-    final SharedPreferences sPref = getPreferenceManager().getSharedPreferences();
-
-    final String timeValue = sPref.getString(getString(R.string.pref_reminder_time_key), "");
-
-    if (model.validateReminderKeyValue(timeValue)) {
-      final int[] timePart = model.splitReminderKeyValue(timeValue);
-      final boolean reminderValue = sPref.getBoolean(getString(R.string.pref_reminder_key), true);
-      ops.updateReminderTime(reminderValue, timePart[0], timePart[1]);
-    } else {
-      Log.e(TAG, "updateNotificationReminder: invalid timeValue[" + timeValue
-                 + "]. forcefully get a correct value and use it.");
-      handlePreferenceClickReminder();
-    }
-  }
+*/
 
   private class ChangeHandler
       implements SharedPreferences.OnSharedPreferenceChangeListener {
@@ -311,12 +199,10 @@ public class SettingsScreen
     @Override
     public void onSharedPreferenceChanged(final SharedPreferences preferences,
                                           final String key) {
-      if (getString(R.string.pref_theme_key).equalsIgnoreCase(key)) {
-        ops.applyThemeSelectedInPreference();
-        ops.restartApp();
-      } else if (getString(R.string.pref_reminder_key).equalsIgnoreCase(key)
-                 || getString(R.string.pref_reminder_time_key).equalsIgnoreCase(key)) {
-        updateNotificationReminder();
+      if (key.equalsIgnoreCase(getString(R.string.pref_theme_key))) {
+        handleValueChangeTheme(key);
+      } else if (key.equalsIgnoreCase(getString(R.string.pref_reminder_key))) {
+        handleValueChangeReminder(key);
       } else {
         Log.e(TAG, "onSharedPreferenceChanged: unhandled key [" + key + "]");
       }
@@ -330,28 +216,7 @@ public class SettingsScreen
     @Override
     public boolean onPreferenceTreeClick(final Preference preference) {
       final String key = preference.getKey();
-
-      if (getString(R.string.pref_theme_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickTheme();
-      } else if (getString(R.string.pref_export_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickExport();
-      } else if (getString(R.string.pref_reminder_time_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickReminder();
-      } else if (getString(R.string.pref_rate_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickRate();
-      } else if (getString(R.string.pref_email_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickEmail();
-      } else if (getString(R.string.pref_about_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickAbout();
-      } else if (getString(R.string.pref_license_key).equalsIgnoreCase(key)) {
-        handlePreferenceClickLicense();
-      } else if (getString(R.string.pref_reminder_key).equalsIgnoreCase(key)
-                 || getString(R.string.pref_reminder_time_key).equalsIgnoreCase(key)) {
-        Log.d(TAG, "onPreferenceTreeClick: ignore intentionally, handled in standalone method");
-      } else {
-        Log.e(TAG, "onPreferenceTreeClick: unknown key[" + key + "]");
-        return false;
-      }
+      Log.d(TAG, "onPreferenceTreeClick: preferenceKey = [" + key + "]");
       return true;
     }
 
--- simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ops/SimpleBibleOps.java
+++ simplebible/src/main/java/com/andrewchelladurai/simplebible/ui/ops/SimpleBibleOps.java
@@ -1,7 +1,6 @@
 package com.andrewchelladurai.simplebible.ui.ops;
 
 import androidx.annotation.IdRes;
-import androidx.annotation.IntRange;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -22,9 +21,5 @@ public interface SimpleBibleOps {
   void shareText(@NonNull String text);
 
   void showMessage(@NonNull String string, @IdRes int anchorViewId);
-
-  void updateReminderTime(boolean enableReminder,
-                          @IntRange(from = 0, to = 23) final int hour,
-                          @IntRange(from = 0, to = 59) final int min);
 
 }
